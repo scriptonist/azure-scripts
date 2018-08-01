e3fd27e04f36b22c15b0b5ea03da3705ca12ea47
scriptonist <aravindkp@outlook.in>
2018-08-01 17:11:07 +0530

    :dizzy: Intial commit, helper scripts to create a new AKS cluster

---
+++ create_cluster.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]
+then
+  printf "%s\n" "Usage:" "$0 <cluster_name> <rg_name>"
+  exit
+fi
+
+echo "Creating cluster $1 in $2"
+
+az aks create -n $1 -g $2 --enable-rbac --node-count 1 --kubernetes-version 1.10.5 --generate-ssh-keys
